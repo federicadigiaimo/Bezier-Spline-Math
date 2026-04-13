421b04987e4f191cf2f5901e3ef3909740d1c05f
Federica Di Giaimo <federica.digiaimo@gmail.com>
2026-04-13 12:48:52 +0200

    Update and rename ShaderMaker.h to src/ShaderMaker.h

---
+++ src/ShaderMaker.h
@@ -0,0 +1,10 @@
+#pragma once
+class ShaderMaker
+{
+    public:
+		static GLuint createProgram(char* vertexfilename, char *fragmentfilename);
+		static char* readShaderSource(const char* shaderFile);
+	
+    private:
+		ShaderMaker() { }	
+};
